c4e08408c143ef7000d97f42d795c51d80056710
Fan Kai Ming <101549255+pdax-kaiming@users.noreply.github.com>
2021-11-18 20:16:47 +1300

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/auth/";
 
-// this has 4 functions for authentication and authorization.
 
 const signup = (username, email, password) => {
     console.log('hi this is authservice sign up');
